683e617df2ab3579ec1b3a36a7b71b5cb5c2dac1
CarlosMerma08 <40005066+CarlosMerma@users.noreply.github.com>
2020-04-11 11:58:16 -0500

    Primo.py (#1)
    * Create Primo.py
    
    * Update Primo.py
    
    * Create primo.txt
    
    * Update primo.txt

---
+++ Primo.py
@@ -0,0 +1,10 @@
+a = input("inserta un numero >> ")
+numero = int(a)
+contador = 0
+for i in range(1,numero+1):
+    if (numero% i)==0:
+        contador = contador + 1
+
+if contador==2:
+    print "el numero es primo"
+else print "el numero no es primo"
